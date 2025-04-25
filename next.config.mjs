dd56fc98ac8535a755e7f0b2c55f8256620d659a
devteam-tc <devteam.tc@gmail.com>
2025-04-25 16:46:29 +0530

    nextconfig file updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,29 +2,15 @@
 const nextConfig = {
     output: 'export', // Enables static export
     trailingSlash: true, // Adds trailing slashes to URLs
-    experimental: {
-      dynamicParams: false, // Ensures all slugs are pre-defined
-    },
     images: {
       unoptimized: true, // Required for next/image in static export
-      
     },
-    
-    // Custom headers for controlling crawler access
-    async headers() {
-      return [
-        {
-          source: '/_next/static/(.*)', // Pattern to match all static files including chunks
-          headers: [
-            {
-              key: 'X-Robots-Tag',
-              value: 'index, follow', // Prevent crawling of chunk URLs
-            },
-          ],
-        },
-      ];
+    typescript: {
+      ignoreBuildErrors: false,
     },
+    experimental: {
+      typedRoutes: true,
+    }
+};
   
-  };
-  
-  export default nextConfig;
+export default nextConfig;
