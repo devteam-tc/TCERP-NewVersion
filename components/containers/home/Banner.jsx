341329bfce467188d0cd5e83c3245570097c12e1
devteam-tc <devteam.tc@gmail.com>
2025-06-11 17:16:34 +0530

    Your descriptive message for these changes

--- components/containers/home/Banner.jsx
+++ components/containers/home/Banner.jsx
@@ -44,12 +44,12 @@ const Banner = () => (
         {/* Rocket */}
         <div className="rocket-center">
           <Image 
-            src="/images/banner/icons/rocket.png" 
+            src="/images/banner/icons/Rocketimg.gif" 
             alt="Rocket" 
-            width={180} 
-            height={180}
+            width={480} 
+            height={480}
             priority={true}
-            quality={85}
+            quality={100}
             placeholder="blur"
             blurDataURL="data:image/webp;base64,UklGRkAAAABXRUJQVlA4IDQAAADwAQCdASoQABAABUB8JZwAAp1K3W4AA/v6yAAAA"
             sizes="(max-width: 768px) 100vw, 380px"
